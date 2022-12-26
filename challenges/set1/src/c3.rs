b40da730e930c20737dbfbd476cad4328e07619a
Ashutosh Varma <ashutoshvarma11@live.com>
2022-12-26 13:00:03 +0530

    feat(set1.3): done

--- challenges/set1/src/c3.rs
+++ challenges/set1/src/c3.rs
@@ -2,15 +2,29 @@ use enscoring::TextMetric;
 
 #[test]
 fn set1_c3_xor_key() {
-    let mut encoded =
+    let encoded =
         hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
             .unwrap();
 
-    for c in (b'a'..b'z').chain(b'A'..b'Z') {
-        let mut encoded_copy = encoded.clone();
-        let decoded = encoded_copy.xor_by_char(c as char);
-        let ltr = String::from_utf8(decoded.as_ref().to_vec()).unwrap();
-        let metric = TextMetric::score(&ltr);
-        println!("{} - {} - {:?}", c as char, ltr, metric.most_frequent_alphabet);
-    }
+    let (key, _) = (b'a'..b'z')
+        .chain(b'A'..b'Z')
+        // we use fold to reduce the iterator into max (key, score) tuple.
+        .fold((char::MAX, -1.0), |(mut max_c, mut max_score), c| {
+            let mut encoded_copy = encoded.clone();
+            let decoded = encoded_copy.xor_by_char(c as char);
+            let ltr = std::str::from_utf8(decoded.as_ref())
+                // make it safe to use unwrap here
+                .or::<std::str::Utf8Error>(Ok(""))
+                .unwrap();
+            let score = ltr.text_score();
+
+            if max_score <= score {
+                max_c = c as char;
+                max_score = score;
+            }
+
+            (max_c, max_score)
+        });
+
+    assert_eq!(key, 'X');
 }
--- packages/enscoring/src/lib.rs
+++ packages/enscoring/src/lib.rs
@@ -1,39 +1,79 @@
 use std::collections::HashMap;
 
-type CharFrequency = HashMap<char, usize>;
+type CharFrequency = HashMap<char, f64>;
 
-fn get_char_frequency(value: &str) -> (CharFrequency, Option<char>) {
-    value
+fn get_char_frequency<F: Fn(char) -> bool>(
+    value: &str,
+    char_filter: F,
+) -> (CharFrequency, Vec<char>) {
+    let count = value.chars().filter(|&c| char_filter(c)).count();
+    let mut res = value
         .chars()
-        .fold((HashMap::new(), None), |(mut map, mut max), c| {
+        .fold((HashMap::new(), Vec::new()), |(mut map, mut max), c| {
             let key = map.entry(c);
-            *key.or_insert(0) += 1;
+            *key.or_insert(0f64) += 1f64 / count as f64;
 
-            if let Some(m) = max {
-                if map.get(&m) < map.get(&c) && c.is_ascii_alphabetic() {
-                    max = Some(c);
+            if char_filter(c) {
+                if max.len() == 0 {
+                    max.push(c);
+                } else if map.get(&max[0]) < map.get(&c) {
+                    max = vec![c];
+                } else if map.get(&max[0]) == map.get(&c) && !max.contains(&c) {
+                    max.push(c);
                 }
-            } else if c.is_ascii_alphabetic() {
-                max = Some(c)
             }
             (map, max)
-        })
+        });
+    res.0.iter_mut().for_each(|(_, v)| {
+        *v *= 100f64;
+    });
+    res
 }
 
-impl TextMetric {
+impl Metric {
     pub fn score(value: &str) -> Self {
-        let (fq, max) = get_char_frequency(value);
+        let (fq, max) = get_char_frequency(value, |_| true);
+        let mut naive_score = 0f64;
+
+        for (c, f) in fq.iter() {
+            if c.is_alphabetic() {
+                naive_score += f;
+            }
+        }
+
+        // if there are no whitespaces and not a single word (approx)
+        //  then reduce the score by 10%.
+        if fq.get(&' ').is_none() && value.len() > 10 {
+            naive_score *= 0.9;
+        }
+
         Self {
             char_frequency: fq,
             most_frequent_alphabet: max,
+            score: naive_score,
         }
     }
 }
 
 #[derive(Debug)]
-pub struct TextMetric {
+pub struct Metric {
     pub char_frequency: CharFrequency,
-    pub most_frequent_alphabet: Option<char>,
+    pub most_frequent_alphabet: Vec<char>,
+    pub score: f64,
+}
+
+pub trait TextMetric {
+    fn text_score(&self) -> f64;
+    fn text_metric(&self) -> Metric;
+}
+
+impl<T: AsRef<str>> TextMetric for T {
+    fn text_score(&self) -> f64 {
+        self.text_metric().score
+    }
+    fn text_metric(&self) -> Metric {
+        Metric::score(self.as_ref())
+    }
 }
 
 #[cfg(test)]
@@ -43,7 +83,6 @@ mod tests {
     #[test]
     fn test_text_scoring() {
         let value = "Alternatively, to keep it out of the workspace, add the package to the";
-        let metric = TextMetric::score(value);
-        dbg!(metric);
+        dbg!(value.text_score());
     }
 }
