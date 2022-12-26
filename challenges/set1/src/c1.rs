2b1a12ab9dbbd3e48366fd688262ba799d7cfa14
Ashutosh Varma <ashutoshvarma11@live.com>
2022-12-26 10:48:44 +0530

    feat(set1.1): done

---
+++ challenges/set1/src/c1.rs
@@ -0,0 +1,9 @@
+#[test]
+fn set1_c1_base64() {
+    let hex = hex::decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap();
+
+    assert_eq!(
+        naivebase64::encode(hex.data()),
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    );
+}
