c813e55d13c2a8de80920a1236b0c2d7f26b0ea2
Ashutosh Varma <ashutoshvarma11@live.com>
2022-12-27 13:01:54 +0530

    feat(set1.6): add base64 decode

--- packages/naivebase64/src/lib.rs
+++ packages/naivebase64/src/lib.rs
@@ -6,6 +6,18 @@ static BASE64_TABLE: [char; 64] = [
     '5', '6', '7', '8', '9', '+', '/',
 ];
 
+fn base64_char_value(v: char) -> anyhow::Result<u8> {
+    match v {
+        'A'..='Z' => Ok(v as u8 - 65),
+        'a'..='z' => Ok(v as u8 - 97 + 26),
+        '0'..='9' => Ok(v as u8 - 48 + 52),
+        '+' => Ok(62),
+        '/' => Ok(63),
+        '=' => Ok(0),
+        _ => Err(anyhow::anyhow!("Not a valid Base64 char - {v}")),
+    }
+}
+
 fn calculate_base64_len_padding(data_len: usize) -> usize {
     let (q, r) = (data_len / 3, data_len % 3);
     (q * 4) + (3 - r)
@@ -62,6 +74,27 @@ pub fn encode<T: AsRef<[u8]>>(value: T) -> String {
     res
 }
 
+pub fn decode<T: AsRef<[u8]>>(value: T) -> anyhow::Result<Vec<u8>> {
+    let data = value.as_ref();
+    let mut res = Vec::with_capacity((data.len() / 4) * 3);
+    for chunk in data.chunks_exact(4) {
+        let b0 = base64_char_value(chunk[0] as char)?;
+        let b1 = base64_char_value(chunk[1] as char)?;
+        let b2 = base64_char_value(chunk[2] as char)?;
+        let b3 = base64_char_value(chunk[3] as char)?;
+
+        res.push((b0 << 2) + (b1 >> 4));
+        if chunk[2] != b'=' {
+            res.push((b1 << 4) + (b2 >> 2));
+            if chunk[3] != b'=' {
+                res.push((b2 << 6) + b3);
+            }
+        }
+    }
+
+    Ok(res)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,11 +108,24 @@ mod tests {
         assert_eq!(a, b, "asserting for length {}", N);
     }
 
+    fn base64_decode(data: &str) {
+        let a = base64::decode(data.clone()).unwrap();
+        let b = decode(data).unwrap();
+
+        assert_eq!(a, b, "assetion for decode, data={data}, a={a:?}, b={b:?}");
+    }
+
     #[test]
     fn test_base64_encode() {
         base64_encode::<100>();
         base64_encode::<101>();
         base64_encode::<102>();
         base64_encode::<104>();
+    }
+
+    #[test]
+    fn test_base64_decode() {
+        // let data = "cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0=";
+        base64_decode("cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0wMA==");
     }
 }
